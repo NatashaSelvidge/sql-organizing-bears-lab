deb09adc128d9bac37e42adb00264d3d3a694a55
Natasha Selvidge <Natasha.selvidge@gmail.com>
2020-12-07 17:24:44 -0800

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES 
+(1, "Mr. Chocolate", 4, "M", "Black", "happy", 1),
+(2, "Rowdy",         5, "M", "Brown", "intense", 1),
+(3, "Tabitha",       6, "F", "Black", "Nice", 1),
+(4, "Sergeant Brown",7, "M", "Brown", "goofy", 1),
+(5, "Melissa",       4, "F", "Brown", "funny", 1),
+(6, "Grinch",        8, "M", "Black", "Grinchy", 1),
+(7, "Wendy",         9, "F", "Black", "naive", 0),
+(8, null,            4, "M", "Black", "aggressive", 1);
+
